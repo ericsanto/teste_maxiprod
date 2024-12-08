5a1db944a5b1a1e709edc7dbb1a7457a00168f8e
ericsanto <ericjesus403@gmail.com>
2024-12-08 13:13:58 -0300

    feat: Registra urls dos app authentication e financial_transaction

--- maxiprod/urls.py
+++ maxiprod/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('authentication.urls')),
-    path('api/v1/', include('person.urls'))
+    path('api/v1/', include('financial_transaction.urls'))
 ]
