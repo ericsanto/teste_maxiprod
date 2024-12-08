fc381c32c900bf12edcce77a01bf6dfab87a7443
ericsanto <ericjesus403@gmail.com>
2024-12-08 13:07:25 -0300

    feat: Cria views para cadastro e listagem de finanças

--- financial_transaction/views.py
+++ financial_transaction/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from .models import FinancialTransaction
+from .serializers import FinancialTransactionModelSerializer
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class FinancialTransactionListAPIView(ListAPIView):
+    serializer_class = FinancialTransactionModelSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):      
+        financial_user = FinancialTransaction.objects.filter(user=self.request.user.id)
+        return financial_user
+
+class FinancialTransationCreateAPIView(CreateAPIView):
+    serializer_class = FinancialTransactionModelSerializer
+    permission_classes = [IsAuthenticated]
+
+
