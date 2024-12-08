a58febeb50eaf45d9b3fca07580d1db588a704b8
ericsanto <ericjesus403@gmail.com>
2024-12-08 17:26:53 -0300

    doc: Documenta a utilidade da funão create

--- financial_transaction/serializers.py
+++ financial_transaction/serializers.py
@@ -8,7 +8,9 @@ class FinancialTransactionModelSerializer(serializers.ModelSerializer):
         model = FinancialTransaction
         fields = ['id', 'methods_payment', 'expenses_category', 'expense', 'date', 'description']  # Sem o campo "user"
         read_only_fields = ['user']
-
+    
+    """atribui o id do usuário logado ao campo 'user'. Para que não precise ser passado
+    no corpo da requisição"""
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
         return super().create(validated_data)
