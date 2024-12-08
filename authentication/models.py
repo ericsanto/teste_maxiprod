38e5b91fc20d55c8d4ece3a1f98be1094b1349b8
ericsanto <ericjesus403@gmail.com>
2024-12-08 17:22:39 -0300

    feat: Criando usuário baseado no User do framework

--- authentication/models.py
+++ authentication/models.py
@@ -1,1 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class UserCustom(AbstractUser):
+    age = models.CharField(max_length=3)
+    date_of_birth = models.DateField()
 
