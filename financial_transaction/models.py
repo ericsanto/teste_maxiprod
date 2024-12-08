67a50aaf74e7db58222394c0e46c1aa52dc23c72
ericsanto <ericjesus403@gmail.com>
2024-12-08 13:06:10 -0300

    feat: Cria model de finanças dos usuários

--- financial_transaction/models.py
+++ financial_transaction/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import choices
+from django.utils.regex_helper import Choice
 
-# Create your models here.
+
+EXPENSE_CATEGORIES = (
+        ('ACADEMIA', 'ACADEMIA'),
+        ('SAÚDE', 'SAÚDE'),
+        ('SALÃO DE BELEZA', 'SALÃO DE BELEZA'),
+        ('ALIMENTAÇÃO', 'ALIMENTAÇÃO'),
+        ('ROUPAS', 'ROUPAS'),
+        ('CAMA MESA E BANHO', 'CAMA, MESA E BANHO'),
+        ('ELETRODOMÉSTICO', 'ELETRODOMÉSTICO'),
+        ('CONTA DE ÁGUA', 'CONTA DE ÁGUA'),
+        ('CONTA DE LUZ', 'CONTA DE LUZ'),
+        ('ALUGUEL', 'ALUGUEL')
+        )
+
+MATHODS_PAYMENT = (
+        ('DINHEIRO', 'DINHEIRO'),
+        ('CARTÃO', 'CARTÃO'),
+        )
+
+class FinancialTransaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    methods_payment = models.CharField(choices=MATHODS_PAYMENT, max_length=255)
+    expenses_category = models.CharField(choices=EXPENSE_CATEGORIES, max_length=255)
+    expense = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    description = models.CharField(max_length=255, blank=True)
+
