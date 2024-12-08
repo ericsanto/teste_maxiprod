aa128974bd9ebf1ba65533c6ea9dbdca99fce656
ericsanto <ericjesus403@gmail.com>
2024-12-08 17:24:47 -0300

    fix: Altera o model da queryset para UserCustom

--- authentication/views.py
+++ authentication/views.py
@@ -3,13 +3,13 @@ from django.db.models import query
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.views import APIView
-from django.contrib.auth.models import User
 from .serializers import RegisterSerializer, UserSerializer
+from .models import UserCustom
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
 
 class UserListAPIView(generics.ListAPIView):
-    queryset = User.objects.all()
+    queryset = UserCustom.objects.all()
     serializer_class = UserSerializer 
