45d6af6e8de6aae4f11f4936af3884ec87e6ce85
ericsanto <ericjesus403@gmail.com>
2024-12-08 13:10:43 -0300

    feat: Cria views de registro de usuário e cria views de autenticação refresh de totoken

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,15 @@
+from re import A
+from django.db.models import query
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
 from django.contrib.auth.models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserSerializer
 
 class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer 
