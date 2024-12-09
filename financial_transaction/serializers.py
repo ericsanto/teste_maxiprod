ea751f88d4c937a775ab63c14411727a92fe477f
ericsanto <ericjesus403@gmail.com>
2024-12-09 10:09:58 -0300

    feat: Adiciona field type_expense no json de retorno

--- financial_transaction/serializers.py
+++ financial_transaction/serializers.py
@@ -3,10 +3,11 @@ from .models import FinancialTransaction
 
 
 class FinancialTransactionModelSerializer(serializers.ModelSerializer):
+    expense = serializers.DecimalField(max_digits=10, decimal_places=2)
     
     class Meta:
         model = FinancialTransaction
-        fields = ['id', 'methods_payment', 'expenses_category', 'expense', 'date', 'description']  # Sem o campo "user"
+        fields = ['id', 'methods_payment', 'expenses_category', 'expense', 'date', 'description', 'type_expense']  # Sem o campo "user"
         read_only_fields = ['user']
     
     """atribui o id do usuário logado ao campo 'user'. Para que não precise ser passado
