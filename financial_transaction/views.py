9232f30ba467aae3081f63f1f3eab12cf7d83782
ericsanto <ericjesus403@gmail.com>
2024-12-08 22:42:11 -0300

    feat: Implementa verificação. Se o usuário tiver menor que 18 anos, ele não pode cadastrar transações financeiras como receita

--- financial_transaction/views.py
+++ financial_transaction/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import status
+from rest_framework.mixins import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
 from .models import FinancialTransaction
 from .serializers import FinancialTransactionModelSerializer
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
@@ -15,7 +18,12 @@ class FinancialTransactionListAPIView(ListAPIView):
 class FinancialTransationCreateAPIView(CreateAPIView):
     serializer_class = FinancialTransactionModelSerializer
     permission_classes = [IsAuthenticated]
-
+    
+    def post(self, request, *args, **kwargs):
+        if int(self.request.user.age) < 18 and request.data.get('type_expense') == 'RECEITA':
+            return Response({"detail": "Método receita é permitido somente para pessoas acima de 18 anos!"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        return super().post(request, *args, **kwargs)
 
 class FinancialTransactionDestroyAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = FinancialTransactionModelSerializer
