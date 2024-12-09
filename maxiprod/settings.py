82bff3fb6a005cc87c875b7f843cbe5d38d21cb3
ericsanto <ericjesus403@gmail.com>
2024-12-09 20:12:21 -0300

    fix: Substitui o HOST do db para localhost

--- maxiprod/settings.py
+++ maxiprod/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': 'teste',
         'USER': 'teste',
         'PASSWORD': 'teste',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': 5432
     }
 }
