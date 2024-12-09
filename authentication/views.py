572beeae4e51f9a7f9c5a54709c1f037adee995a
ericsanto <ericjesus403@gmail.com>
2024-12-09 10:51:28 -0300

    feat: Criando view para atualização de usuário

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,11 @@
 from re import A
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import PermissionDenied
 from django.db.models import query
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer, UserSerializer
-from .models import UserCustom
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -14,3 +14,21 @@ class RegisterView(generics.CreateAPIView):
 class UserListAPIView(generics.ListAPIView):
     queryset = get_user_model().objects.prefetch_related('transactions')
     serializer_class = UserSerializer
+
+
+class UserUpdateAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = get_user_model()
+    serializer_class = UserSerializer
+    
+    #recupera o objeto que está vindo da requisição
+    def get_object(self):
+        obj = super().get_object()
+
+        if obj.id != self.request.user.id:
+            raise PermissionDenied('Você não pode alterar dados de outro usuário')
+
+        return obj
+
+
+
